0a43c6fde5c407d0ee14be8725a66adea7661770
BrunoMadera <brunomadera@gmail.com>
2023-11-28 15:38:32 -0300

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,14 @@
 function NavBar() {
   return (
     
-        <h1>Componente Navbar</h1>
+    <div className="containerNavBar">
+        <div id="logo"> logo</div>
+        <div id="nome"> nome</div>
+        <div id="link1"> link1</div>
+        <div id="link2"> link2</div>
+        <div id="link3"> link3</div>
+        <div id="link4"> link4</div>
+    </div>
  
   );
 }
